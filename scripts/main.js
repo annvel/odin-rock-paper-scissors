965327a1534f83bea6ba5c58211e21fdaed055ef
Ann Kiende <41993451+annvel@users.noreply.github.com>
2023-03-27 17:08:36 +0300

    Add scripts directory
    the folder has main.js
    the js file has the pseudocode

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+// Pseudocode
+// 1. Computer randomly plays "Rock", "Paper" or "Scissors"
+// 2. User inputs "Rock", "Paper" or "Scissors"
+// 3. Compare both player selections and assign a winner
+// 4. If Rock and Paper are played, Paper wins
+// 5. If Rock and Scissors are played, Rock wins
+// 6. If Paper and Scissors are played, Scissors wins
+// 7. If both selection are same, game is tie
+// 8. Ensure Player's selection input is case insensitive
+// 9, Ensure Computer's selection input is case insensitive
+// 10. Play 5 game rounds and determine the final winner
